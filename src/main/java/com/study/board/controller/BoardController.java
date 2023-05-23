baa4673b4567e52a48880acf90d1e2073e4f4aa9
Seungho <bc1206@naver.com>
2023-05-24 02:09:48 +0900

    Feat : 글 작성 후 DB 저장 기능 추가

--- src/main/java/com/study/board/controller/BoardController.java
+++ src/main/java/com/study/board/controller/BoardController.java
@@ -1,14 +1,32 @@
 package com.study.board.controller;
 
+import ch.qos.logback.core.net.SyslogOutputStream;
+import com.study.board.entity.Board;
+import com.study.board.service.BoardService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class BoardController{
+
+    @Autowired
+    private BoardService boardService;
+
     @GetMapping("/board/write")//어떤 url로 접근할건지에 대해 지정해주는 어노테이션 (localhost:8080/board/write 접속)
     public String boardWriteForm(){
 
         return "boardwrite"; //boardwrite로 보내줌 (어떤 뷰 파일로 보내줄지)
     }
+
+    @PostMapping("/board/writepro")
+    public String boardWritePro(Board board){
+
+        boardService.write(board);
+
+        return "";
+    }
+
 }
---
+++ src/main/java/com/study/board/entity/Board.java
@@ -0,0 +1,17 @@
+package com.study.board.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class Board {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id; //mysql 형식에 맞게
+    private String title;
+    private String content;
+}
