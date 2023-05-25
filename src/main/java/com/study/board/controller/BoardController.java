193a69526809d0e2eb57a282f7b84810f5d9749c
Seungho <bc1206@naver.com>
2023-05-25 22:56:34 +0900

    Feat : 게시글 삭제 기능 추가

--- src/main/java/com/study/board/controller/BoardController.java
+++ src/main/java/com/study/board/controller/BoardController.java
@@ -3,6 +3,7 @@ package com.study.board.controller;
 import ch.qos.logback.core.net.SyslogOutputStream;
 import com.study.board.entity.Board;
 import com.study.board.service.BoardService;
+import jakarta.persistence.criteria.CriteriaBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -35,6 +36,23 @@ public class BoardController{
 
         model.addAttribute("list",boardService.boardList());
         return "boardlist";
+    }
+
+    //localhost:8080/board/view?id=1
+    @GetMapping("/board/view")
+    public String boardView(Model model, Integer id){
+
+        model.addAttribute("board", boardService.boardView(id));
+
+        return "boardview";
+    }
+
+    //특정게시글 삭제
+    @GetMapping("/board/delete")
+    public String boardDelete(Integer id){
+        boardService.boardDelete(id);
+
+        return "redirect:/board/list"; //삭제하고 가지는 페이지 리턴 값으로 설정
     }
 
 }
